af640300ff4ac3ff5d112a3f5e9ccc03f54e5d5f
unknown <yurimarcelo2018@gmail.com>
2023-02-07 21:13:37 -0300

    Estudando Sobre Import e Export em java script

---
+++ moduloA.js
@@ -0,0 +1,12 @@
+const variavel = "Valor"
+const variavel2 = "outro valor";
+
+/* export default variavel ; */
+
+export default {
+    variavel: variavel,
+    variavel2 : variavel2,
+};
+
+export const variavel3 = "valor3";
+export const variavel4 = "valor4";
---
+++ moduloB.js
@@ -0,0 +1,5 @@
+import variavel from "./moduloA.js";
+import { variavel3, variavel4 } from "./moduloA.js";
+
+console.log(variavel);
+console.log(variavel3 + " e " + variavel4);
